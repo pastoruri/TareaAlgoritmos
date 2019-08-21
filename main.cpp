6332029158d7cbaf68fad45bc5a0b0287f8a16db
Rodrigo <rodrigo.cespedes@utec.edu.pe>
2019-08-21 16:29:18 -0500

    Fisrt commit

---
+++ main.cpp
@@ -0,0 +1,44 @@
+#include<iostream>
+
+
+using namespace std;
+
+void bubblesort(int arr[],int arr_len);
+
+int main(){
+
+int arreglo_de_numeros[10]={3,8,1,6,2,7,9,5,6,10};
+
+bubblesort(arreglo_de_numeros, 10);
+
+for(int i=0;i<10;i++){
+
+	cout<<arreglo_de_numeros[i]<<endl;
+
+}
+
+return 0;
+
+}
+
+
+void bubblesort(int arr[],int arr_len){
+
+	for(int i=0;i<arr_len;i++){
+	
+		for(int j=0;j<arr_len-i-1;j++){
+	
+		
+			if(arr[j]>arr[j+1]){
+			
+				int temp=arr[j];
+			
+				arr[j]=arr[j+1];
+		
+				arr[j+1]=temp;
+		}
+	}
+}
+}
+
+
